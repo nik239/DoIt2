1334b521e610b419265f1d5dc66c523518b77dd3
nik239 <116445208+nik239@users.noreply.github.com>
2024-01-02 11:26:54 -0500

    minor re-organizing

--- DoIt2/Persistence/SortPreferences.swift
+++ DoIt2/Persistence/SortPreferences.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+//
 enum ToDosSorts: String, CaseIterable {
   case dateNewest
   case dateOldest
--- DoIt2/View Models/PersistedDiffableDataSource.swift
+++ DoIt2/View Models/PersistedDiffableDataSource.swift
@@ -5,24 +5,24 @@
 //  Created by Nikita Ivanov on 15/12/2023.
 //
 
-import UIKit
-import CoreData
-
-class PersistedDiffableDataSource: UITableViewDiffableDataSource<String, NSManagedObjectID> {
-  
-}
-
-extension PersistedDiffableDataSource {
-}
-
-
-//MARK: -NSFetchedResultsControllerDelegate
-extension PersistedDiffableDataSource: NSFetchedResultsControllerDelegate {
-  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
-                  didChangeContentWith
-                  snapshot: NSDiffableDataSourceSnapshotReference) {
-    let snapshot = snapshot
-    as NSDiffableDataSourceSnapshot<String, NSManagedObjectID>
-    self.apply(snapshot)
-  }
-}
+//import UIKit
+//import CoreData
+//
+//class PersistedDiffableDataSource: UITableViewDiffableDataSource<String, NSManagedObjectID> {
+//  
+//}
+//
+//extension PersistedDiffableDataSource {
+//}
+//
+//
+////MARK: -NSFetchedResultsControllerDelegate
+//extension PersistedDiffableDataSource: NSFetchedResultsControllerDelegate {
+//  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+//                  didChangeContentWith
+//                  snapshot: NSDiffableDataSourceSnapshotReference) {
+//    let snapshot = snapshot
+//    as NSDiffableDataSourceSnapshot<String, NSManagedObjectID>
+//    self.apply(snapshot)
+//  }
+//}
